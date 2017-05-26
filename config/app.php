90114fd4c5315cc3c3e2a57ada830ae0695fdbab
santi <santiagoloperam@gmail.com>
2017-05-26 11:02:59 -0500

    Jhonatan actualizo rama con estilos, ajax y js en Admin

--- config/app.php
+++ config/app.php
@@ -167,9 +167,6 @@ return [
          * Package Service Providers...
          */
 
-        /// mis providers
-          Collective\Html\HtmlServiceProvider::class,
-
         //
 
         /*
@@ -228,10 +225,6 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-
-        // Mis alises
-        'Form' => Collective\Html\FormFacade::class,
-        'Html' => Collective\Html\HtmlFacade::class,
 
     ],
 
