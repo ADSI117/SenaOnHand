47163685a46a5be8f7e77a060d2fb87f5be21b16
Jhonathan Steven Calderon Sanchez <jhonathan19920912@outlook.com>
2017-06-10 00:17:25 -0500

    SOHv 2.1.1a

--- resources/views/main-panel/publicaciones/crear.blade.php
+++ resources/views/main-panel/publicaciones/crear.blade.php
@@ -3,6 +3,9 @@
 @include('template.h-navbar')
 
 @section('main')
+
+
+  {!! Form::open(['route' => 'publicaciones.store', 'method' => 'POST', 'files' => true, 'class' => '']) !!}
   <div class="page-banner">
     <div class="container">
       <h1>Nueva publicacion</h1>
@@ -13,48 +16,42 @@
       </button>
     </div>
   </div>
-
-  {!! Form::open(['route' => 'publicaciones.store', 'method' => 'POST', 'files' => true, 'class' => 'container']) !!}
-  <div class="row justify-content-center">
-
-
-    <div class="w-100"></div>
-
-
-    <div class="col-sm-12 col-md-6">
-      <div class="form-group form-group-no-border">
-        {!! Form::label('titulo', 'Titulo') !!}
-        {!!Form::text('titulo', null, ['id' => 'nombre', 'placeholder' => 'Título', 'required', 'class' => 'form-control'])!!}
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-sm-12 col-md-6">
+        <div class="form-group form-group-no-border">
+          {!! Form::label('titulo', 'Titulo') !!}
+          {!!Form::text('titulo', null, ['id' => 'nombre', 'placeholder' => 'Título', 'required', 'class' => 'form-control'])!!}
+        </div>
+        <div class="form-group form-group-no-border ">
+          {!! Form::label('contenido', 'Contenido') !!}
+          {!!Form::textarea('contenido', null, ['id' => 'contenido', 'rows' => '5', 'placeholder' => 'Contenido de la pubicación', 'required', 'class' => 'form-control'])!!}
+        </div>
+        <div class="form-group form-group-no-border">
+          {!! Form::select('subcategoria_id', $subcategorias, null, ['placeholder' => 'Seleccione una Subcategoria', 'required', 'class' => 'form-control']) !!}
+        </div>
+        <div class="form-group form-group-no-border">
+          {!! Form::select('estado_id', $estados, null, ['placeholder' => 'Estado publicacion', 'required', 'class' => 'form-control']) !!}
+        </div>
+        <div class="form-group form-group-no-border">
+          {!! Form::select('tags', $tags, null, ['multiple'=>'multiple', 'name'=>'tags[]',  'required', 'class' => 'form-control']) !!}
+        </div>
       </div>
-      <div class="form-group form-group-no-border ">
-        {!! Form::label('contenido', 'Contenido') !!}
-        {!!Form::textarea('contenido', null, ['id' => 'contenido', 'rows' => '5', 'placeholder' => 'Contenido de la pubicación', 'required', 'class' => 'form-control'])!!}
-      </div>
-      <div class="form-group form-group-no-border">
-        {!! Form::select('subcategoria_id', $subcategorias, null, ['placeholder' => 'Seleccione una Subcategoria', 'required', 'class' => 'form-control']) !!}
-      </div>
-      <div class="form-group form-group-no-border">
-        {!! Form::select('estado_id', $estados, null, ['placeholder' => 'Estado publicacion', 'required', 'class' => 'form-control']) !!}
-      </div>
-      <div class="form-group form-group-no-border">
-        {!! Form::select('tags', $tags, null, ['multiple'=>'multiple', 'name'=>'tags[]',  'required', 'class' => 'form-control']) !!}
+      <div class="col-sm-12 col-md-6">
+        <div class="form-group form-group-no-border ">
+          {!! Form::label('imagen', 'Imagen') !!}
+          {!! Form::file('imagen', ['class' => 'form-control']) !!}
+        </div>
+        <div class="form-group form-group-no-border ">
+          {!! Form::label('archivo', 'Archivo') !!}
+          {!! Form::file('archivo', ['class' => 'form-control']) !!}
+        </div>
+
+        <div class="form-group form-group-no-border">
+          {!! Form::label('video', 'Video') !!}
+          {!!Form::text('video', null, ['id' => 'video', 'placeholder' => 'URL del video', 'class' => 'form-control'])!!}
+        </div>
       </div>
     </div>
-    <div class="col-sm-12 col-md-6">
-      <div class="form-group form-group-no-border ">
-        {!! Form::label('imagen', 'Imagen') !!}
-        {!! Form::file('imagen', ['class' => 'form-control']) !!}
-      </div>
-      <div class="form-group form-group-no-border ">
-        {!! Form::label('archivo', 'Archivo') !!}
-        {!! Form::file('archivo', ['class' => 'form-control']) !!}
-      </div>
-
-      <div class="form-group form-group-no-border">
-        {!! Form::label('video', 'Video') !!}
-        {!!Form::text('video', null, ['id' => 'video', 'placeholder' => 'URL del video', 'class' => 'form-control'])!!}
-      </div>
-    </div>
-
     {!! Form::close()!!}
   @endsection
