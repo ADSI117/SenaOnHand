5d047d17972f893fd0d735d11f185e365ba5fcf2
santi <santiagoloperam@gmail.com>
2017-06-10 14:46:29 -0500

    para que las migraciones corrieran se corrigio el campo user_id por usuario_id y en 2 modelos que afectaba esta sintaxis

--- database/migrations/2017_05_31_171834_add_imagenes_table.php
+++ database/migrations/2017_05_31_171834_add_imagenes_table.php
@@ -34,3 +34,4 @@ class AddImagenesTable extends Migration
         Schema::dropIfExists('imagenes');
     }
 }
+
--- database/migrations/2017_05_31_171941_add_videos_table.php
+++ database/migrations/2017_05_31_171941_add_videos_table.php
@@ -34,3 +34,4 @@ class AddVideosTable extends Migration
         Schema::dropIfExists('videos');
     }
 }
+
