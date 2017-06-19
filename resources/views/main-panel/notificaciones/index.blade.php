6a02382772d5b9c69586f1a7851a3fc69135d7fb
Diego Suarez <diego-jbs@hotmail.com>
2017-06-18 23:45:36 -0500

    Notificaciones

--- resources/views/main-panel/notificaciones/index.blade.php
+++ resources/views/main-panel/notificaciones/index.blade.php
@@ -6,44 +6,81 @@
 
 @section('main')
   <div class="container">
-    <h3>Sus notificaciones</h3>
     <div class="row">
-      <div class="col-md-6">
-        <h4>No leidas</h4>
-        <div class="list-group">
-          @foreach($noleidas as $noleida)
-            <div class="list-group-item">
-              <!-- {{ var_dump($noleida->data) }} -->
-              <a href="{{$noleida->data['link']}}">{{$noleida->data['text']}}</a>
 
-              {!!Form::open(['route'=>['notificaciones.update', $noleida], 'method' => 'POST'])!!}
-                {{ method_field('PATCH') }}
-                {{ csrf_field() }}
-                {!!Form::submit('x', ['class' => 'btn btn-danger btn-xs'])!!}
-              {!!Form::close()!!}
-
-            </div>
-          @endforeach
-        </div>
-
-
-      </div>
-
-      <div class="col-md-6">
-        <h4>Leidas</h4>
-        <div class="list-group">
-          @foreach($leidas as $leida)
-            <a href="{{$leida->data['link']}}">{{$leida->data['text']}}</a>
-
-            {!!Form::open(['route'=>['notificaciones.destroy', $leida->id], 'method' => 'DELETE'])!!}
-
-              {!!Form::submit('x', ['class' => 'btn btn-danger btn-xs'])!!}
-            {!!Form::close()!!}
-
-          @endforeach
-        </div>
-
+      <div class="col">
+          <!-- <p class="category">Tabs with Icons on Card</p> -->
+          <!-- Nav tabs -->
+          <div class="card">
+              <ul class="nav nav-tabs nav-tabs-indigo justify-content-center" role="tablist">
+                  <li class="nav-item">
+                      <a class="nav-link active" data-toggle="tab" href="#noleidas" role="tab">
+                          <i class="fa fa-envelope"></i> No leidas
+                      </a>
+                  </li>
+                  <li class="nav-item">
+                      <a class="nav-link" data-toggle="tab" href="#leidas" role="tab">
+                          <i class="fa fa-envelope-open"></i> Leidas
+                      </a>
+                  </li>
+              </ul>
+              <div class="card-block">
+                  <!-- Tab panes -->
+                  <div class="tab-content">
+                      <div class="tab-pane active" id="noleidas" role="tabpanel">
+                        @foreach($noleidas as $noleida)
+                          <a href="#" data-rol="noleido" class="material-card">
+                            <small class="content-date">
+                              <i class="fa fa-clock-o mr-2"></i> {{$noleida->created_at}}
+                            </small>
+                            <div class="content-header-icon bl-red">
+                              <div class="header--icon">
+                                <i class="red fa fa-envelope-o fa-3x"></i>
+                              </div>
+                            </div>
+                            <div class="material-card-header">
+                              <div class="content-header-title">
+                                <h5 class="header--title">{{$noleida->data['text']}}</h5>
+                              </div>
+                            </div>
+                          </a>
+                          {!!Form::open(['route'=>['notificaciones.update', $noleida], 'method' => 'POST'])!!}
+                            {{method_field('PATCH')}}
+                            <input type="hidden" name="enlace" value="{{$noleida->data['link']}}" />
+                            {{csrf_field()}}
+                          {!!Form::close()!!}
+                        @endforeach
+                      </div>
+                      <div class="tab-pane" id="leidas" role="tabpanel">
+                        @foreach($leidas as $leida)
+                          <a href="#" data-rol="leido" data-id="{{$leida->id}}" class="material-card">
+                            <small class="content-date">
+                              <i class="fa fa-clock-o mr-2"></i> {{$noleida->created_at}}
+                            </small>
+                            <div class="content-header-icon bl-red">
+                              <div class="header--icon">
+                                <i class="red fa fa-times fa-3x"></i>
+                              </div>
+                            </div>
+                            <div class="material-card-header">
+                              <div class="content-header-title">
+                                <h5  class="header--title">{{$leida->data['text']}}</h5>
+                              </div>
+                            </div>
+                          </a>
+                          {!!Form::open(['route'=>['notificaciones.destroy', $leida->id], 'method' => 'DELETE'])!!}
+                          <input type="hidden" name="enlace" value="{{$leida->data['link']}}" />
+                          {!!Form::close()!!}
+                        @endforeach
+                      </div>
+                  </div>
+              </div>
+          </div>
       </div>
     </div>
   </div>
+@endsection
+
+@section('js')
+  <script src="{{asset('js/main/notificaciones.js')}}"></script>
 @endsection
--- resources/views/main-panel/salas-chats/index.blade.php
+++ resources/views/main-panel/salas-chats/index.blade.php
@@ -22,11 +22,9 @@
   <div class="row">
     <div class="col full-h">
 
-
-
-      <div class="list-group">
         @foreach($salas as $sala)
         <a href="{{route('salas.show', $sala->id)}}" class="material-card">
+          <small class="content-date"><i class="fa fa-clock-o mr-2"></i> {{$sala->updated_at}}</small>
           <div class="material-card-header">
             @if(Auth::user()->id == $sala->usuario_amigo_id)
               <img src="{{Storage::url($sala->user_creador->url_foto)}}"  alt="">
@@ -37,32 +35,15 @@
               <div class="content-header-title">
                 <h5  class="header--title">Con {{$sala->user_amigo->nombres}} {{$sala->user_amigo->apellidos}}</h5>
             @endif
-              <small class="header--date">Último mensaje: {{$sala->updated_at}}</small>
             </div>
           </div>
 
 
         </a>
-        <!-- <a href="{{route('salas.show', $sala->id)}}" class="list-group-item list-group-item-action flex-column align-items-start">
-          <div class="d-flex w-100 align-items-center">
-            @if(Auth::user()->id == $sala->usuario_amigo_id)
-              <img src="{{Storage::url($sala->user_creador->url_foto)}}" class="rounded-circle" width="75" height="75" alt="">
-              <h5  class="mb-1">Con {{$sala->user_creador->nombres}} {{$sala->user_creador->apellidos}}</h5 >
-            @else
-              <img src="{{Storage::url($sala->user_amigo->url_foto)}}" class="rounded-circle" width="75" height="75" alt="">
-              <h5  class="mb-1">Con {{$sala->user_amigo->nombres}} {{$sala->user_amigo->apellidos}}</h5 >
-            @endif
-            <small class="align-self-start">Último mensaje: {{$sala->updated_at}}</small>
-          </div>
-        </a> -->
         @endforeach
-      </div>
 
     </div>
   </div>
 </div>
-
-
-
 
 @endsection
