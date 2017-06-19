4a2d3a6c39d2818877db0eb044cd33541c583065
Diego Suarez <diego-jbs@hotmail.com>
2017-06-19 02:28:34 -0500

    Nevas carsds categorias

--- resources/views/main-panel/notificaciones/index.blade.php
+++ resources/views/main-panel/notificaciones/index.blade.php
@@ -5,82 +5,82 @@
 @include('template.h-navbar')
 
 @section('main')
-  <div class="container">
-    <div class="row">
+	<div class="container">
+		<div class="row">
 
-      <div class="col">
-          <!-- <p class="category">Tabs with Icons on Card</p> -->
-          <!-- Nav tabs -->
-          <div class="card">
-              <ul class="nav nav-tabs nav-tabs-indigo justify-content-center" role="tablist">
-                  <li class="nav-item">
-                      <a class="nav-link active" data-toggle="tab" href="#noleidas" role="tab">
-                          <i class="fa fa-envelope"></i> No leidas
-                      </a>
-                  </li>
-                  <li class="nav-item">
-                      <a class="nav-link" data-toggle="tab" href="#leidas" role="tab">
-                          <i class="fa fa-envelope-open"></i> Leidas
-                      </a>
-                  </li>
-              </ul>
-              <div class="card-block">
-                  <!-- Tab panes -->
-                  <div class="tab-content">
-                      <div class="tab-pane active" id="noleidas" role="tabpanel">
-                        @foreach($noleidas as $noleida)
-                          <a href="#" data-rol="noleido" class="material-card">
-                            <small class="content-date">
-                              <i class="fa fa-clock-o mr-2"></i> {{$noleida->created_at}}
-                            </small>
-                            <div class="content-header-icon bl-red">
-                              <div class="header--icon">
-                                <i class="red fa fa-envelope-o fa-3x"></i>
-                              </div>
-                            </div>
-                            <div class="material-card-header">
-                              <div class="content-header-title">
-                                <h5 class="header--title">{{$noleida->data['text']}}</h5>
-                              </div>
-                            </div>
-                          </a>
-                          {!!Form::open(['route'=>['notificaciones.update', $noleida], 'method' => 'POST'])!!}
-                            {{method_field('PATCH')}}
-                            <input type="hidden" name="enlace" value="{{$noleida->data['link']}}" />
-                            {{csrf_field()}}
-                          {!!Form::close()!!}
-                        @endforeach
-                      </div>
-                      <div class="tab-pane" id="leidas" role="tabpanel">
-                        @foreach($leidas as $leida)
-                          <a href="#" data-rol="leido" data-id="{{$leida->id}}" class="material-card">
-                            <small class="content-date">
-                              <i class="fa fa-clock-o mr-2"></i> {{$noleida->created_at}}
-                            </small>
-                            <div class="content-header-icon bl-red">
-                              <div class="header--icon">
-                                <i class="red fa fa-times fa-3x"></i>
-                              </div>
-                            </div>
-                            <div class="material-card-header">
-                              <div class="content-header-title">
-                                <h5  class="header--title">{{$leida->data['text']}}</h5>
-                              </div>
-                            </div>
-                          </a>
-                          {!!Form::open(['route'=>['notificaciones.destroy', $leida->id], 'method' => 'DELETE'])!!}
-                          <input type="hidden" name="enlace" value="{{$leida->data['link']}}" />
-                          {!!Form::close()!!}
-                        @endforeach
-                      </div>
-                  </div>
-              </div>
-          </div>
-      </div>
-    </div>
-  </div>
+			<div class="col">
+					<!-- <p class="category">Tabs with Icons on Card</p> -->
+					<!-- Nav tabs -->
+					<div class="card">
+							<ul class="nav nav-tabs nav-tabs-red justify-content-center" role="tablist">
+									<li class="nav-item">
+											<a class="nav-link active" data-toggle="tab" href="#noleidas" role="tab">
+													<i class="fa fa-envelope"></i> No leidas
+											</a>
+									</li>
+									<li class="nav-item">
+											<a class="nav-link" data-toggle="tab" href="#leidas" role="tab">
+													<i class="fa fa-envelope-open"></i> Leidas
+											</a>
+									</li>
+							</ul>
+							<div class="card-block">
+									<!-- Tab panes -->
+									<div class="tab-content">
+											<div class="tab-pane active" id="noleidas" role="tabpanel">
+												@foreach($noleidas as $noleida)
+													<a href="#" data-rol="noleido" class="material-card">
+														<small class="content-date">
+															<i class="fa fa-clock-o mr-2"></i> {{$noleida->created_at}}
+														</small>
+														<div class="content-header-icon bl-red">
+															<div class="header--icon">
+																<i class="red fa fa-envelope-o fa-3x"></i>
+															</div>
+														</div>
+														<div class="material-card-header">
+															<div class="content-header-title">
+																<h5 class="header--title">{{$noleida->data['text']}}</h5>
+															</div>
+														</div>
+													</a>
+													{!!Form::open(['route'=>['notificaciones.update', $noleida], 'method' => 'POST'])!!}
+														{{method_field('PATCH')}}
+														<input type="hidden" name="enlace" value="{{$noleida->data['link']}}" />
+														{{csrf_field()}}
+													{!!Form::close()!!}
+												@endforeach
+											</div>
+											<div class="tab-pane" id="leidas" role="tabpanel">
+												@foreach($leidas as $leida)
+													<a href="#" data-rol="leido" data-id="{{$leida->id}}" class="material-card">
+														<small class="content-date">
+															<i class="fa fa-clock-o mr-2"></i> {{$noleida->created_at}}
+														</small>
+														<div class="content-header-icon bl-red">
+															<div class="header--icon">
+																<i class="red fa fa-times fa-3x"></i>
+															</div>
+														</div>
+														<div class="material-card-header">
+															<div class="content-header-title">
+																<h5  class="header--title">{{$leida->data['text']}}</h5>
+															</div>
+														</div>
+													</a>
+													{!!Form::open(['route'=>['notificaciones.destroy', $leida->id], 'method' => 'DELETE'])!!}
+													<input type="hidden" name="enlace" value="{{$leida->data['link']}}" />
+													{!!Form::close()!!}
+												@endforeach
+											</div>
+									</div>
+							</div>
+					</div>
+			</div>
+		</div>
+	</div>
 @endsection
 
 @section('js')
-  <script src="{{asset('js/main/notificaciones.js')}}"></script>
+	<script src="{{asset('js/main/notificaciones.js')}}"></script>
 @endsection
--- resources/views/main-panel/suscripciones/index.blade.php
+++ resources/views/main-panel/suscripciones/index.blade.php
@@ -3,38 +3,71 @@
 @include('template.h-navbar')
 
 @section('main')
-  <div class="container">
-    <div class="row">
-      <div class="col-xs-12 col-md-6">
-        <h2>Mis categorías</h2>
+	<div class="container">
+		<div class="row">
+			<div class="col">
+			<div class="card">
+					<ul class="nav nav-tabs nav-tabs-indigo justify-content-center" role="tablist">
+						<li class="nav-item">
+							<a class="nav-link active" data-toggle="tab" href="#noleidas" role="tab">
+								<i class="fa fa-list-alt"></i> Mis categorías
+							</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" data-toggle="tab" href="#leidas" role="tab">
+								<i class="fa fa-graduation-cap"></i> Instructores
+							</a>
+						</li>
+					</ul>
+					<div class="card-block">
+						<!-- Tab panes -->
+						<div class="tab-content">
+							<div class="tab-pane active" id="noleidas" role="tabpanel">
+								<div class="row">
+									@foreach($usuario->categorias as $categoria)
+									<div class="col-xs-12 col-md-6 col-lg-4">
+										{{ Form::open(['method' => 'DELETE', 'class' => 'material-card__big', 'route' => ['categoria-usuario.destroy', $categoria->id]]) }}
+											<div class="card-header__image" style="background-image: url(http://www.popcomunicaciones.com/blog/media/k2/items/cache/e0a70f72bdae9885bfc32d7cd19a26a1_Generic.jpg)">
+												<h2 class="card-header__titulo">{{$categoria->descripcion}}</h2>
+											</div>
+											<p class="card__text">
+												Located two hours south of Sydney in the Southern Highland of New South Wales...
+											</p>
+											<div class="card__action-bar">
+												<botton type="submit" class="card__button">
+													Dejar de seguir											
+												</botton>
+											</div>
+										{{ Form::close() }}
+									</div>
+									@endforeach
+								</div>
+							
+							</div>
+							<div class="tab-pane" id="leidas" role="tabpanel">
+							
+							@foreach($usuario->seguidos as $seguido)
+								{{ Form::open(['method' => 'DELETE', 'route' => ['seguidos.destroy', $seguido->id]]) }}
+								@if($seguido->seguido->url_foto)
+								<img src="{{ Storage::url($seguido->seguido->url_foto) }}" alt="" width="50" height="50">
+								@else
+								<img src="{{url('/')}}/imagenes/perfiles/soh_profile_default.png" alt="" width="50" height="50">
+								@endif
+								<a href="#">{{$seguido->seguido->nombres}} {{$seguido->seguido->apellidos}}</a>
 
-        @foreach($usuario->categorias as $categoria)
-          {{ Form::open(['method' => 'DELETE', 'route' => ['categoria-usuario.destroy', $categoria->id]]) }}
-          <a href="#">{{$categoria->descripcion}}</a>
-          {!!Form::submit('Dejar de seguir', ['class'=>'btn btn-warning'])!!}
-          {{ Form::close() }}
-        @endforeach
+								{!!Form::submit('Dejar de seguir', ['class'=>'btn btn-warning'])!!}
+								{{ Form::close() }}
+							@endforeach
 
-      </div>
-    </div>
-
-    <div class="row">
-      <div class="col-xs-12 col-md-6">
-        <h2>Instructores</h2>
-
-        @foreach($usuario->seguidos as $seguido)
-          {{ Form::open(['method' => 'DELETE', 'route' => ['seguidos.destroy', $seguido->id]]) }}
-          @if($seguido->seguido->url_foto)
-            <img src="{{ Storage::url($seguido->seguido->url_foto) }}" alt="" width="50" height="50">
-          @else
-            <img src="{{url('/')}}/imagenes/perfiles/soh_profile_default.png" alt="" width="50" height="50">
-          @endif
-          <a href="#">{{$seguido->seguido->nombres}} {{$seguido->seguido->apellidos}}</a>
-
-          {!!Form::submit('Dejar de seguir', ['class'=>'btn btn-warning'])!!}
-          {{ Form::close() }}
-        @endforeach
-      </div>
-    </div>
-  </div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 @endsection
+
+
+
+ 
