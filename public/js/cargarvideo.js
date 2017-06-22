fd3765e7b1ed401d675a5ceab87c38ad7dcdf220
Jhonathan <jhonatan19921209@gmail.com>
2017-06-22 13:32:21 -0500

    Url Video

--- public/js/cargarvideo.js
+++ public/js/cargarvideo.js
@@ -2,8 +2,22 @@
 
     const CONTAINER = document.querySelector('#video-container');
 
-    document.querySelector('#video').addEventListener('change', function(e) {
-        CONTAINER.innerHTML = Video( this.value );
+    document.querySelector('#video').addEventListener('input', function(e) {
+        // https://www.youtube.com/watch?v=lmvUFhjZdFc
+        // https://www.youtube.com/embed/lmvUFhjZdFc
+        if (this.value != "") {
+            let uri = this.value.split('?v=');
+            var embed = "";
+
+            if (uri[0] == "https://www.youtube.com/watch") {
+                embed = "https://www.youtube.com/embed/" + uri[1];
+            } else {
+                embed = uri[0];
+            }
+            console.log(embed)
+
+            CONTAINER.innerHTML = Video( embed );
+        }
     });
 
     function Video(uri) {
